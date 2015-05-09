f41e97225541980b77bc93bdc62a07b5e969ea20
pdgonzalez872 <pdgonzalez872@gmail.com>
2015-05-09 14:23:20 -0500

    PG

--- Dreams_for_Kids/application_associate_board.py
+++ Dreams_for_Kids/application_associate_board.py
@@ -21,6 +21,9 @@ class DreamsForKids:
         self.file_path = c.file_path
         self.gs_sheet_add = c.gs_sheet_add
         self.gs_key = c.gs_key
+        self.sender = c.sender
+        self.gmail_password = c.gmail_password
+        self.recipient_list = c.recipient_list
 
         os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = "{}\{}".format(self.file_path,
                                                                       "DreamsForKids-0661d09ef1fc.json")
@@ -76,7 +79,8 @@ class DreamsForKids:
 
         list_of_lists = worksheet.get_all_values()
 
-        print(list_of_lists)
+        # print(list_of_lists)
+
         return list_of_lists
 
     def create_pdf_application_associate_board_reportlab(self, person_details):
@@ -205,6 +209,54 @@ class DreamsForKids:
                              person_details[12], \
                              person_details[13]
 
+    def email_response_update(self, recipient_list, responses):
+        """
+        Gives periodic updates on the people who have signed up for the associate
+        board. Will be a scheduled task on Paulo's machine, will run every 30 min.
+
+        Source:
+        http://robertwdempsey.com/python3-email-with-attachments-using-gmail/
+        """
+        import smtplib
+        from email.mime.multipart import MIMEMultipart
+        from email.mime.text import MIMEText
+        from email.mime.image import MIMEImage
+        from email.mime.base import MIMEBase
+        from email import encoders
+        import datetime
+
+        sender = self.sender
+        gmail_password = self.gmail_password
+
+        # Create the enclosing (outer) message
+        outer = MIMEMultipart()
+        fmt = datetime.datetime.now().strftime("%b-%d-%Y  -  %H:%M")
+        outer['Subject'] = "Associate Board Responses {}".format(fmt)
+        outer['To'] = recipient_list
+        outer['From'] = sender
+        outer.preamble = 'You will not see this in a MIME-aware mail reader.\n'
+
+        # attach the text
+        text = "There are currently {} people who have signed up.".format(responses)
+        part1 = MIMEText(text, 'plain')
+        outer.attach(part1)
+
+        composed = outer.as_string()
+
+        # Send the email
+        try:
+            s = smtplib.SMTP('smtp.gmail.com', 587)
+            s.ehlo()
+            s.starttls()
+            s.ehlo()
+            s.login(sender, gmail_password)
+            s.sendmail(sender, recipient_list, composed)
+            s.close()
+            # print("Email sent!")
+        except:
+            # print("Unable to send the email. Error: ", sys.exc_info()[0])
+            raise
+
     def main(self):
         """
         Puts everything together
@@ -216,8 +268,14 @@ class DreamsForKids:
         # for itm in data_list_csv[1:]:  # Skips header
         #     d.create_pdf_application_associate_board_reportlab(itm)
 
-        # # Google Sheets data
-        # data_list_gs = d.unpacks_data_google_sheets()
+        # Google Sheets data
+        data_list_gs = d.unpacks_data_google_sheets()
+
+        responses = len(data_list_gs) - 1
+        # print(len(data_list_gs))
+
+        d.email_response_update(recipient_list=self.recipient_list,
+                                responses=responses)
 
         # # Creates the pdfs via Google Sheets - ok
         # for itm in data_list_gs[1:]:  # Skips header
