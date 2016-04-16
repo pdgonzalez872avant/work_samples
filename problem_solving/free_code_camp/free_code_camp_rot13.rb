11466768a7e87081a0abef1a5eb0fc56e1c5c17e
pdgonzalez872 <pdgonzalez872@gmail.com>
2016-04-16 15:23:40 -0500

    Refactor: use inject istead of each

--- problem_solving/free_code_camp/free_code_camp_rot13.rb
+++ problem_solving/free_code_camp/free_code_camp_rot13.rb
@@ -46,18 +46,21 @@ def hash_table
 end
 
 def translate_string(split_string:, rules:)
-  translated_string = ''
-
-  split_string.each do |letter|
+  split_string.inject('') do |translated_string, letter|
     translated_string += rules[letter]
   end
-  translated_string
 end
 
-raise "Bigfoot" unless rot13("HELLO") == "URYYB"
+p 'Starting tests'
+
+raise "Not working Yet" unless rot13("HELLO") == "URYYB"
+p 'passed'
 raise "Not Working Yet" unless rot13("FREE CODE CAMP") == "SERR PBQR PNZC"
+p 'passed'
 raise "Not Working Yet" unless rot13("SERR CVMMN!") == "FREE PIZZA!"
+p 'passed'
 raise "Not Working Yet" unless rot13("SERR YBIR?") == "FREE LOVE?"
+p 'passed'
 raise "Not Working Yet" unless rot13("GUR DHVPX OEBJA QBT WHZCRQ BIRE GUR YNML SBK.") == "THE QUICK BROWN DOG JUMPED OVER THE LAZY FOX."
 
 p "All passed"
