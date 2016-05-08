e33b92ec1f9992b2ac5bf4015e7c83a801993b64
pdgonzalez872 <pdgonzalez872@gmail.com>
2016-05-07 19:08:08 -0500

    Add guards

--- elixir_etudes/etude_3.ex
+++ elixir_etudes/etude_3.ex
@@ -7,16 +7,21 @@ defmodule Geom do
   This is the doc. I wonder how to print the docs for functions.
   """
 
-  def area(:rectangle, length, width) do
+  def area(:rectangle, length, width) when length >= 0 and width >= 0 do
     length * width
   end
 
-  def area(:triangle, base, height) do
+  def area(:triangle, base, height) when base >= 0 and height >= 0 do
     (base * height) / 2
   end
 
-  def area(:ellipse, major, minor) do
+  def area(:ellipse, major, minor) when major >= 0 and minor >= 0 do
     :math.pi() * major * minor
+  end
+
+  # this catches all things :)
+  def area(_, _, _) do
+    0
   end
 end
 
@@ -34,3 +39,10 @@ Test.assert(Geom.area(:triangle, 3, 5), 7.5)
 Test.assert(Geom.area(:ellipse, 2, 4), 25.132741228718345)
 
 IO.puts "All tests passed"
+
+Test.assert(Geom.area(:rectangle, -3, 4), 0)
+Test.assert(Geom.area(:pentagon, 3, 4), 0)
+Test.assert(Geom.area(:ellipse, -1, 5), 0)
+Test.assert(Geom.area(:triangle, 5, -1), 0)
+
+IO.puts "All tests passed, with guard"
