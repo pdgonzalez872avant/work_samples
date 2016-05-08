7a64bf36b03f11534981666a707403c6c253e07a
pdgonzalez872 <pdgonzalez872@gmail.com>
2016-05-07 19:23:14 -0500

    Add private methods

--- elixir_etudes/etude_3.ex
+++ elixir_etudes/etude_3.ex
@@ -7,20 +7,24 @@ defmodule Geom do
   This is the doc. I wonder how to print the docs for functions.
   """
 
-  def area(:rectangle, length, width) when length >= 0 and width >= 0 do
+  def area({shape, a, b}) do
+    area(shape, a, b)
+  end
+
+  defp area(:rectangle, length, width) when length >= 0 and width >= 0 do
     length * width
   end
 
-  def area(:triangle, base, height) when base >= 0 and height >= 0 do
+  defp area(:triangle, base, height) when base >= 0 and height >= 0 do
     (base * height) / 2
   end
 
-  def area(:ellipse, major, minor) when major >= 0 and minor >= 0 do
+  defp area(:ellipse, major, minor) when major >= 0 and minor >= 0 do
     :math.pi() * major * minor
   end
 
   # this catches all things :)
-  def area(_, _, _) do
+  defp area(_, _, _) do
     0
   end
 end
@@ -34,15 +38,22 @@ defmodule Test do
   end
 end
 
-Test.assert(Geom.area(:rectangle, 3, 4), 12)
-Test.assert(Geom.area(:triangle, 3, 5), 7.5)
-Test.assert(Geom.area(:ellipse, 2, 4), 25.132741228718345)
+#Test.assert(Geom.area(:rectangle, 3, 4), 12)
+#Test.assert(Geom.area(:triangle, 3, 5), 7.5)
+#Test.assert(Geom.area(:ellipse, 2, 4), 25.132741228718345)
 
 IO.puts "All tests passed"
 
-Test.assert(Geom.area(:rectangle, -3, 4), 0)
-Test.assert(Geom.area(:pentagon, 3, 4), 0)
-Test.assert(Geom.area(:ellipse, -1, 5), 0)
-Test.assert(Geom.area(:triangle, 5, -1), 0)
+#Test.assert(Geom.area(:rectangle, -3, 4), 0)
+#Test.assert(Geom.area(:pentagon, 3, 4), 0)
+#Test.assert(Geom.area(:ellipse, -1, 5), 0)
+#Test.assert(Geom.area(:triangle, 5, -1), 0)
 
 IO.puts "All tests passed, with guard"
+
+Test.assert(Geom.area({:rectangle, 7, 3}), 21)
+Test.assert(Geom.area({:triangle, 7, 3}), 10.5)
+Test.assert(Geom.area({:ellipse, 7, 3}), 65.97344572538566)
+Test.assert(Geom.area({:pentagon, 7, 3}), 0)
+
+IO.puts "All tests passed with tuple stuff"
