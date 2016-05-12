99b718f07e2c9bf2d37044cd2af51cd8790d358f
pdgonzalez872 <pdgonzalez872@gmail.com>
2016-05-11 21:18:13 -0500

    hm

--- problem_solving/project_euler/elixir/project_euler_001.ex
+++ problem_solving/project_euler/elixir/project_euler_001.ex
@@ -3,13 +3,15 @@ defmodule Problem_001 do
   def valid?(e) do
     if (rem(e, 3) == 0) || (rem(e, 5) == 0) do
       true
+    else
+      false
     end
   end
 
   def main(range) do
     Enum.reduce(range, 0, fn(e, acc) ->
       if valid?(e) do
-        IO.puts(e)
+        IO.puts(acc)
       end
     end)
   end
@@ -24,9 +26,9 @@ defmodule Test do
 end
 
 Test.assert(Problem_001.valid?(3), true)
-Test.assert(Problem_001.valid?(5), true)
+Test.assert(Problem_001.valid?(4), false)
 Test.assert(Problem_001.valid?(9), true)
-Problem_001.main(1..9)
+
 
 #f = fn(x) -> ((rem(x, 3) == 0) || (rem(x, 5) == 0)) end
 #result = Enum.filter((1..9), f)
