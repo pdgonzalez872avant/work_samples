bd39b3894528c2c39e244b53b2900d895b9f74e8
pdgonzalez872 <pdgonzalez872@gmail.com>
2016-05-14 22:28:13 -0500

    Finish all? enum

--- ruby_etudes/enumerables_spec.rb
+++ ruby_etudes/enumerables_spec.rb
@@ -2,16 +2,12 @@ require 'rspec'
 
 describe Enumerable do
   context 'all?' do
-
-    let(:strings) { ['longerstring', 'short', 'no', 'yeeeeah'] }
-
-    context 'takes a collection and passes each element of the collection to a block' do
+    context 'takes a collection and passes each element of the collection to a block. Halts whenever condition is not met' do
       context 'numbers' do
         it 'returns true if all elements are integers and greater than 0' do
+          all_integers = [1, 2, 3, 4]
 
-          integers = [1, 2, 3, 4]
-
-          result = integers.all? do |e|
+          result = all_integers.all? do |e|
             e.is_a?(Integer) && e > 0
           end
           expect(result).to be true
@@ -23,15 +19,45 @@ describe Enumerable do
           result = not_all_floats.all? { |e| e.is_a?(Float) }
           expect(result).to be false
         end
-
       end
 
-      context 'strings'
+      context 'strings' do
+        it 'can evaluate strings based on length' do
+          strings = ['longerstring', 'short', 'no', 'yeeeeah']
 
-      context 'edge case -> nils'
+          result = strings.all? { |e| e.length >= 2 }
+          expect(result).to be true
+        end
+
+        it 'can evaluate strings based on content' do
+          all_have_a = ['langarstrang', 'shart', 'na', 'yaaaaah']
+
+          result = all_have_a.all? { |e| e.include?('a') }
+          expect(result).to be true
+        end
+      end
+
+      context 'truthy assumptions' do
+        it 'defaults to truthy values if block is not passed' do
+          truthy_values = [1, 2, 'yes', true]
+
+          result = truthy_values.all?
+          expect(result).to be true
+        end
+
+        it 'defaults to truthy values if block is not passed and returns false if there is a falsy value' do
+          falsy_spy_among_truthy_values = [1, 2, 'yes', nil]
+
+          result = falsy_spy_among_truthy_values.all?
+          expect(result).to be false
+        end
+      end
     end
-  end
-end
+  end # all?
+
+  context 'any?'
+
+end # Enumerable
 
 all_enumerables = [ 'all?',
                     'any?',
